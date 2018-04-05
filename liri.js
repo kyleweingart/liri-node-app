4316897ef5d61878f22d139926b1ff5ee839826f
Kyle Weingart <kyle.weingart@gmail.com>
2018-04-04 23:09:54 -0400

    new logic for movie api

--- liri.js
+++ liri.js
@@ -8,6 +8,12 @@ var Spotify = require('node-spotify-api');
 var request = require('request');
 
 var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify({
+//   id: 58ce6f9803e54a3e98d9c59b14e5e90c,
+//   secret: 2e553892b0404cd98b0439b05c3cdef0
+// });
+
+
 var client = new Twitter(keys.twitter);
 
 
@@ -76,6 +82,31 @@ function spotifySong() {
 }
 
 function thisMovie() {
+  var movieName = process.argv[3];
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+  console.log(queryUrl);
+  console.log(movieName);
+  request(queryUrl, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+  
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log(body);
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+
+    }
+  });
+  
+
 
 }
 
